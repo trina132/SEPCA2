ddf5ad9d8f4981943366848a8c7404aca5745853
Trina132 <trinalim.23@ichat.sp.edu.sg>
2024-07-09 20:57:50 +0800

    test

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var app = require('./controller/app.js');
 
 var server = app.listen(8081, function () {
     var port = server.address().port;
-    console.log('Web App Hosted at http://localhost:%s/B/selectCountry.html', port)
+    console.log('Web App Hosted at http://localhost:%s/B/selectCountry.html', port);
 });
